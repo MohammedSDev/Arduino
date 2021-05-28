48804cbd17a1f66df87e37388df9d674fc4c2de8
m.salem <gg.goo.mobile@gmail.com>
2021-05-28 16:33:46 +0300

    update gradle.

--- app/src/main/java/com/digital/testanythingapplication/MainActivity.kt
+++ app/src/main/java/com/digital/testanythingapplication/MainActivity.kt
@@ -2,9 +2,9 @@ package com.digital.testanythingapplication
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_main.*
 
